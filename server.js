9747a9b96e9f692a952378e4402d1a53fefc13d6
Sandipan Khanra <sandipankhanra@gmail.com>
2024-02-26 17:54:45 +0530

    Unhandled and uncaught rejections code added

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
 const port = process.env.PORT || 8000;
-app.listen(port, (req, res) => {
-  console.log(`App is running on port ${port}`);
+const server = app.listen(port, (req, res) => {
+  console.log(`App is running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  const message = `${err.name}: ${err.message}`;
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", (err) => {
+  const message = `${err.name}: ${err.message}`;
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
